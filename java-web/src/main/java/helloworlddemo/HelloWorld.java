9c58f2335564fe54c558a937d116fac34e2be01f
chenlinxin07 <m19880789869@163.com>
2026-05-22 21:33:15 +0800

    学了maven基础

---
+++ java-web/src/main/java/helloworlddemo/HelloWorld.java
@@ -0,0 +1,9 @@
+package helloworlddemo;
+
+import java.util.logging.Logger;
+
+public class HelloWorld {
+    static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ java-web/src/test/java/test/Demotest.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.Test;
+
+public class Demotest {
+    @Test
+    public void test1() {
+        System.out.println("hello test 1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("hello test 2");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("hello test 3");
+    }
+}
